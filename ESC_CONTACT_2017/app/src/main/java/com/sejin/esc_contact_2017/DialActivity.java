ef1d04e99a9efbfbc395d0afbac1f825edf7c8ae
wickedSejin <ghosejin@hanyang.ac.kr>
2017-07-19 09:01:25 +0900

    onClick 처리해줬고, 지우기 버튼에 setOnLongClick 메소드를 이용해서 롱클릭시에 그 패널이 싹 지워지게 했다.

--- ESC_CONTACT_2017/app/src/main/java/com/sejin/esc_contact_2017/DialActivity.java
+++ ESC_CONTACT_2017/app/src/main/java/com/sejin/esc_contact_2017/DialActivity.java
@@ -1,14 +1,21 @@
 package com.sejin.esc_contact_2017;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class DialActivity extends AppCompatActivity  {
+public class DialActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
     Button btnAsteroid,btn0,btnCrossHatch;
 
+    TextView tvNumberPanel;
+
+    ImageButton btnErase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +37,71 @@ public class DialActivity extends AppCompatActivity  {
         btn7=(Button)findViewById(R.id.button7);
         btn8=(Button)findViewById(R.id.button8);
         btn9=(Button)findViewById(R.id.button9);
+
         btnAsteroid=(Button)findViewById(R.id.button_asteroid);
         btnCrossHatch=(Button)findViewById(R.id.button_Crosshatch);
+        btnErase=(ImageButton)findViewById(R.id.eraseImgBtn);
+        tvNumberPanel=(TextView)findViewById(R.id.numPanel);
+
+        btnErase.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                tvNumberPanel.setText(null);
+                return false;
+            }
+        });
 
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button0:
+                tvNumberPanel.append("0");
+                break;
+            /**  case문 오른쪽에는 상수가 와야해서 getId() 를 사용할 수 없어요.
+             * 빨간줄에 커서 가져다 대면 에러메세지 뜨는데 에러메세지 읽는법 알려주세요 **/
+            //case btn1.getId():
+            case R.id.button1:
+                tvNumberPanel.append("1");
+                break;
+            case R.id.button2:
+                tvNumberPanel.append("2");
+                break;
+            case R.id.button3:
+                tvNumberPanel.append("3");
+                break;
+            case R.id.button4:
+                tvNumberPanel.append("4");
+                break;
+            case R.id.button5:
+                tvNumberPanel.append("5");
+                break;
+            case R.id.button6:
+                tvNumberPanel.append("6");
+                break;
+            case R.id.button7:
+                tvNumberPanel.append("7");
+                break;
+            case R.id.button8:
+                tvNumberPanel.append("8");
+                break;
+            case R.id.button9:
+                tvNumberPanel.append("9");
+                break;
+            case R.id.button_asteroid:
+                tvNumberPanel.append("*");
+                break;
+            case R.id.button_Crosshatch:
+                tvNumberPanel.append("#");
+                break;
+            case R.id.eraseImgBtn:
+                String temp = tvNumberPanel.getText().toString();
+                if (temp.length()> 0) tvNumberPanel.setText(temp.substring(0,temp.length()-1));
+                break;
+//            case R.id.nextActivityImgBtn:
 
-
-
+        }
+    }
 }
